7f1f8782521145b6ac0c8e0dc9bc77793b70d7ec
Rafael Carvalho <dannyelrc@hotmail.com>
2026-08-03 12:45:48 -0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -4,11 +4,9 @@ import os
 
 
 def create_file() -> None:
-
     command = sys.argv[1:]
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    dir_path = None
     dir_args = []
     if "-d" in command:
         d_index = command.index("-d")
@@ -16,32 +14,31 @@ def create_file() -> None:
             if arg.startswith("-"):
                 break
             dir_args.append(arg)
-        if dir_args:
-            dir_path = os.path.join(*dir_args)
-            os.makedirs(dir_path, exist_ok=True)
 
-    if "-f" not in command:
-        return
+    dir_path = None
+    if dir_args:
+        dir_path = os.path.join(*dir_args)
+        os.makedirs(dir_path, exist_ok=True)
 
     file_name = None
-    f_index = command.index("-f")
-    for name in command[f_index + 1:]:
-        if name.startswith("-"):
-            break
-        file_name = name
+    if "-f" in command:
+        f_index = command.index("-f")
+        if f_index + 1 < len(command) and not command[f_index + 1].startswith("-"):
+            file_name = command[f_index + 1]
 
-    if not file_name:
-        return
-
-    file_path = os.path.join(dir_path, file_name) if dir_path else file_name
-    with open(file_path, "a") as f:
-        file_content = []
-        line = 1
-        while True:
-            line_content = input("Enter content line: ")
-            if line_content == "stop":
-                break
-            file_content.append(f"Line{line} {line_content}")
-            line += 1
-        f.write(current_time + "\n")
-        f.write("\n".join(file_content) + "\n")
+    if file_name:
+        file_path = os.path.join(dir_path, file_name) if dir_path else file_name
+        file_exists = os.path.exists(file_path)
+        with open(file_path, "a") as f:
+            file_content = []
+            line = 1
+            while True:
+                line_content = input("Enter content line: ")
+                if line_content == "stop":
+                    break
+                file_content.append(f"{line} {line_content}")
+                line += 1
+            if file_exists:
+                f.write("\n")
+            f.write(current_time + "\n")
+            f.write("\n".join(file_content) + "\n")
